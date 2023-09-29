a047706f391e1ed27edb6e14e4f86e4dc7555fe2
Olgadz469 <133228372+Olgadz469@users.noreply.github.com>
2023-09-29 13:00:11 +0300

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 const BASE_URL = 'https://api.thecatapi.com/v1';
-// axios.defaults.headers.common['x-rapid-api-key'] = 'live_L1TH07Be6qZTtyTEzaE8r7JI8izNYbvHoRZsdak7ttQM0w6Ddvg9CPmNCWXTRdtX';
-const API_KEY = 'live_L1TH07Be6qZTtyTEzaE8r7JI8izNYbvHoRZsdak7ttQM0w6Ddvg9CPmNCWXTRdtX';
+axios.defaults.headers.common['api-key'] = 'live_L1TH07Be6qZTtyTEzaE8r7JI8izNYbvHoRZsdak7ttQM0w6Ddvg9CPmNCWXTRdtX';
+// const API_KEY = 'live_L1TH07Be6qZTtyTEzaE8r7JI8izNYbvHoRZsdak7ttQM0w6Ddvg9CPmNCWXTRdtX';
 
 export function fetchBreeds() {
- return fetch (`${BASE_URL}/breeds?api_key=${API_KEY}`)
-
+//  return fetch (`${BASE_URL}/breeds?api_key=${API_KEY}`)
+ return fetch (`${BASE_URL}/breeds`)
 
   .then (response => {
   if (!response.ok) {
@@ -16,8 +16,8 @@ export function fetchBreeds() {
 };
 
 export function fetchCatByBreed(breedId) {
-  return fetch(`${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`)
-
+  // return fetch(`${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`)
+  return fetch(`${BASE_URL}/images/search?breed_ids=${breedId}`)
 
       .then(response => {
 
