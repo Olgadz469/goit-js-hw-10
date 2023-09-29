f5947c766766d514ba8d95cc143e5c2323974a84
Olgadz469 <133228372+Olgadz469@users.noreply.github.com>
2023-09-29 08:58:11 +0300

    change-1

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://api.thecatapi.com/v1';
 axios.defaults.headers.common['x-rapid-api-key'] = 'live_L1TH07Be6qZTtyTEzaE8r7JI8izNYbvHoRZsdak7ttQM0w6Ddvg9CPmNCWXTRdtX';
 
 export function fetchBreeds() {
-  return fetch (`${BASE_URL}/breeds?api_key=${x-rapid-api-key}`)
+  return fetch (`${BASE_URL}/breeds?api_key=${'x-rapid-api-key'}`)
 
   .then (response => {
   if (!response.ok) {
@@ -14,7 +14,7 @@ export function fetchBreeds() {
 };
 
 export function fetchCatByBreed(breedId) {
-  return fetch(`${BASE_URL}/images/search?api_key=${x-rapid-api-key}&breed_ids=${breedId}`)
+  return fetch(`${BASE_URL}/images/search?api_key=${'x-rapid-api-key'}&breed_ids=${breedId}`)
 
       .then(response => {
 
